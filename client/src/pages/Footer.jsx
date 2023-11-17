57604c8f9b8ccf90fec996f0982242e78531cadd
Thinker92 <vilsonapo@gmail.com>
2023-11-16 19:55:13 -0500

    Added basic header and footer, adjusted main.jsx to match routes

---
+++ client/src/pages/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer className="footer">
+        <p>© 2023 MotoMetrics. All rights reserved.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
