8cbb29247c2aa30056c61646ff04191cc9a1e2b7
Thinker92 <vilsonapo@gmail.com>
2023-11-20 19:47:13 -0500

    Setup SearchForm and basic search page

---
+++ client/src/components/SearchForm.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+
+const SearchForm = () => {
+  const [searchParams, setSearchParams] = useState({
+    make: '',
+    model: '',
+    fuel_type: '',
+    drive: '',
+    cylinders: '',
+    transmission: '',
+    year: '',
+    min_city_mpg: '',
+    max_city_mpg: '',
+    min_hwy_mpg: '',
+    max_hwy_mpg: '',
+    min_comb_mpg: '',
+    max_comb_mpg: '',
+    limit: 10, // Default limit
+  });
+
+  const handleChange = (e) => {
+    setSearchParams({ ...searchParams, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const queryParams = new URLSearchParams(searchParams).toString();
+    const url = `https://api.api-ninjas.com/v1/cars?${queryParams}`;
+    console.log(url);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="make" placeholder="Make (e.g., Audi)" onChange={handleChange} />
+      <input name="model" placeholder="Model (e.g., A4)" onChange={handleChange} />
+      <input name="fuel_type" placeholder="Fuel Type (e.g., gas)" onChange={handleChange} />
+      <input name="drive" placeholder="Drive (e.g., fwd)" onChange={handleChange} />
+      <input name="cylinders" placeholder="Cylinders (e.g., 4)" onChange={handleChange} type="number" />
+      <input name="transmission" placeholder="Transmission (e.g., automatic)" onChange={handleChange} />
+      <input name="year" placeholder="Year (e.g., 2020)" onChange={handleChange} type="number" />
+      <input name="min_city_mpg" placeholder="Min City MPG" onChange={handleChange} type="number" />
+      <input name="max_city_mpg" placeholder="Max City MPG" onChange={handleChange} type="number" />
+      <input name="min_hwy_mpg" placeholder="Min Highway MPG" onChange={handleChange} type="number" />
+      <input name="max_hwy_mpg" placeholder="Max Highway MPG" onChange={handleChange} type="number" />
+      <input name="min_comb_mpg" placeholder="Min Combined MPG" onChange={handleChange} type="number" />
+      <input name="max_comb_mpg" placeholder="Max Combined MPG" onChange={handleChange} type="number" />
+      <input name="limit" placeholder="Result Limit (1-50)" onChange={handleChange} type="number" />
+
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default SearchForm;
